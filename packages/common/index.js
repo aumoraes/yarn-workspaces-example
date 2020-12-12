d62018abed6f51272b31d7e1b4d82d4351932fe9
Aurelio Moraes <aureliodemoraes13@gmail.com>
2020-12-12 18:50:31 -0300

    feat: print current date

--- packages/common/index.js
+++ packages/common/index.js
@@ -1,3 +1,14 @@
 module.exports = () => {
   console.log("hello from common 12");
+
+  let datetime = new Date();
+  // current date
+  // adjust 0 before single digit date
+  let date = ("0" + datetime.getDate()).slice(-2);
+  // current month
+  let month = ("0" + (datetime.getMonth() + 1)).slice(-2);
+  // current year
+  let year = datetime.getFullYear();
+  console.log(year + "-" + month + "-" + date);
+
 };
